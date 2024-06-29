415fc5725c76d3cfe213f650c4fec3bd568eb582
SpacyBiscote <imranejr2704@yahoo.com>
2024-06-30 00:28:56 +0100

    modifcation mineur fihcier App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Housing from './Pages/Housing';
+import Service from './Pages/Service';
+import Contact from './Pages/Contact';
+import Connexion from './Pages/Connexion';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/Housing" element={<Housing/>} />
+          <Route path="/Service" element={<Service/>} />
+          <Route path="/Contact" element={<Contact/>} />
+          <Route path="/Connexion" element={<Connexion/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
