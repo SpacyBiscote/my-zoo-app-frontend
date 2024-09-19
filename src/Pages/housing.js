638bd83a7e4eaf7ef9a7361469ae307cf6fe768b
SpacyBiscote <imranejr2704@yahoo.com>
2024-09-19 15:30:35 +0100

    Ajout des aniamux dans la page habitat

--- src/Pages/housing.js
+++ src/Pages/housing.js
@@ -2,6 +2,17 @@ import React from 'react';
 import '../Styles/Habitat.css';
 import { useState, useEffect } from "react";
 import { click } from "@testing-library/user-event/dist/click";
+const speciesMap = {
+ 
+  2: 'Camelus dromedarius',
+  3: 'Vulpes zerda',
+  4: 'Ailurus fulgens',
+  5: 'Nomascus gabirellae',
+  6: 'Panthera pardus pardus',
+  7: 'Gazella dorcas'
+  
+};
+
 
 
 export default function Housing() {
@@ -9,7 +20,9 @@ export default function Housing() {
   //Recuperer les données Habitat
   const [responsegetHabitat , setResponsegetHabitat] = useState(null);
   const [responsegetanimaux ,  setResponsegetanimaux] = useState(null)
-  const [shownanimal, setShownanimal] = useState({});
+  const [selectedHabitat, setSelectedHabitat] = useState(null);
+
+  const [shownHabitats, setShownHabitats] = useState({});
 
 
   async function  gethabitat() {
@@ -28,46 +41,46 @@ export default function Housing() {
   },[])
 
 
-  
-
-  //Fonction pour le  premier bouton
-  const shownAnimalgg = () => {
-    return <p>Les animaux</p>;
-  };
-  
-
- /* const clickHabitat = (index) => {
-    setShownanimal(index);
-  }*/
 
 
   //récupérer les données d'animaux 
-
-  async function  getanimaux(habitatId) {
-    try {
-        const getAnimaux1 = await fetch("http://localhost:8082/3000/Housing/${habitatId}/animal" ,
-        {credentials : "include" , method : "get" , headers: {'Content-Type': 'application/json'}});
-        const getAnimaux1json = await getAnimaux1.json();
-        setShownanimal(prev => ({ ...prev, [habitatId]: getAnimaux1json }));
-    }catch (error){
-        console.error(error);
-    }
+async function getAnimaux() {
+  try {
+    const getaniamux1 = await fetch("http://localhost:8082/3000/Housing/animaux" , 
+    {credentials : "include" , method : "get" , headers: {'Content-Type': 'application/json'}});
+    const getanimauxjson = await getaniamux1.json();
+    setResponsegetanimaux(getanimauxjson);
+    console.log(getanimauxjson);
+  }catch(error) {
+    console.error(error)
   }
+}
 
+useEffect(()=>{
+  getAnimaux();
+},[])
+
+
+//Fonction pour le  premier bouton
   
+const filterAnimauxByHabitat = (habitatId) => {
+  if (!responsegetanimaux) return [];
+  return responsegetanimaux.filter(animal => animal.detient === habitatId);
+}
 
-  const clickHabitat = async (index, habitatId) => {
-    if (!shownanimal[habitatId]) {
-      await getanimaux(habitatId); 
-    }
-  };
 
-    //  <button className="button-habitat" onClick={() => clickHabitat(index)}>En savoir plus </button>
-      //  {shownanimal === index && shownAnimalgg()}
+const toggleShowAnimals = (habitatId) => {
+  setShownHabitats(prevState => ({
+    ...prevState,
+    [habitatId]: !prevState[habitatId]  // On inverse l'état actuel de l'habitat sélectionné
+  }));
+};
 
+/*() => setSelectedHabitat(habitat.id)*/ //pour le bouton
   if(!responsegetHabitat){
     return <p>Les données sont en cours de chargemement</p>
   }
+    
   return (
     <div className="habitat-container">
         <h1>Nos habitats</h1>
@@ -77,17 +90,27 @@ export default function Housing() {
               <h2>{habitat.nom}</h2>
               <img src={habitat.image_habitat} className="image-habitat"/>
               <p>{habitat.description}</p>
-              <button className="button-habitat" onClick={() => clickHabitat(habitat.id)}>En savoir plus</button>
-               {shownanimal[habitat.id] && (
-                <div>
-                  <h3>Les animaux :</h3>
-                  <ul>
-                  {shownanimal[habitat.id].map((animal, i) => (
-                  <li key={i}>
-                    {animal.prenom} <img src={animal.animal_image} alt={animal.prenom} />
-                  </li>
-                ))}
-              </ul>
+              <button className="button-habitat"  onClick={() => toggleShowAnimals(habitat.id)}>En savoir plus</button>
+               
+
+            {shownHabitats[habitat.id] && (
+            <div className="animaux-container">
+              <h3>Animaux dans cet habitat :</h3>
+              {filterAnimauxByHabitat(habitat.habitat_id).length > 0 ? (
+                filterAnimauxByHabitat(habitat.habitat_id).map((animal, index) => (
+                  <div key={index} className="animal-info">
+                   
+                    <img src={animal.animal_image} alt={animal.prenom} className="animal-image" />
+                    <p>Nom: {animal.prenom}</p>
+                    <p>Race: {speciesMap[animal.dispose]}</p>
+
+
+                 
+                  </div>
+                ))
+              ) : (
+                <p>Aucun animal dans cet habitat pour le moment.</p>
+              )}
             </div>
           )}
               
@@ -100,3 +123,4 @@ export default function Housing() {
     )
     
 };
+
--- src/User/Admins.js
+++ src/User/Admins.js
@@ -4,24 +4,39 @@ import '../Styles/Admins.css'
 
 export default function Admins() {
     //Tous les states pour les forms
+
      //state pour les ajout service
      const [imageajoutservice , setImageajoutservice] = useState('');
      const [nomajoutservice , setNomajoutservice] = useState('');
      const [descriptionajoutservice , setDescriptionajoutservice] = useState('');
-     
-     const handlerChangerimageajservice = (event) => {
-       setImageajoutservice(event.target.value);
-     }
-     const handleChangernomajoutservice = (event) => {
-      setNomajoutservice(event.target.value);
-     }
-     const handleChangerdescriptionajoutservice = (event) => {
-      setDescriptionajoutservice(event.target.value);
-     }
+     //suprimmer servive
+     const [supprimservice , setSupprimservice] = useState('');
 
+     //modifier un service
+     const [imagemodifservice , setImagemodifservice] = useState('');
+     const [nommodifservice , setNommodifservice] = useState('');
+     const [descriptionmodifservice , setDescriptionmodifservice] = useState('');
+     const [anciennom, setAnciennom] = useState('');
+
+
+     //ajouter état service
+     const handlerChangerimageajservice = (event) => {setImageajoutservice(event.target.value);}
+     const handleChangernomajoutservice = (event) => {setNomajoutservice(event.target.value);}
+     const handleChangerdescriptionajoutservice = (event) => {setDescriptionajoutservice(event.target.value);}
+
+     //supprimer état service
+     const handleChangersupprimmservice = (event) => {setSupprimservice(event.target.value);}
+      
+     //modif état service 
+     const handlerChangerimagemoservice = (event) => {setImagemodifservice(event.target.value);}
+    const handleChangernommodifservice = (event) => {setNommodifservice(event.target.value);}
+    const handleChangerdescriptionmodifservice = (event) => {setDescriptionmodifservice(event.target.value);}
+    const handleChangeranciennom = (event) => {setAnciennom(event.target.value);}
+
+     //fetch pour ajouter un servic
     async function Ajoutservice() {
       try{
-        const reponseAjoutservice = await fetch("http://localhost:8082/3000/Admins" , 
+        const reponseAjoutservice = await fetch("http://localhost:8082/3000/Admins/service" , 
           {credentials : "include" ,  method : "post" , headers :  { 'Content-Type': 'application/json'} , 
           body : JSON.stringify({imageajoutservice : imageajoutservice , nomajoutservice : nomajoutservice , descriptionajoutservice : descriptionajoutservice }),})
         const jsonreponseAjoutservice = await reponseAjoutservice.json();
@@ -30,15 +45,96 @@ export default function Admins() {
         console.error(error)
       }
     }
-  
-//travailler ici sur un systeme qui nous permts de modif et suprimer vite 
+   //fetch pour supprimer un service 
+   async function Supprservice() {
+    try{
+      const responsesupprservice = await fetch("http://localhost:8082/3000/Admins/service" , 
+        {credentials : "include" ,  method : "delete" , headers :  { 'Content-Type': 'application/json'},
+        body : JSON.stringify({ supprimservice: supprimservice }), })
+        const jsonresponsesupprservice = await responsesupprservice.json();
+    } catch(error) {
+      console.error(error)
+    } 
+   }
+
+   async function Modifservice() {
+    try{
+      const reponseModifservice = await fetch("http://localhost:8082/3000/Admins/service" , 
+        {credentials : "include" ,  method : "put" , headers :  { 'Content-Type': 'application/json'} , 
+        body : JSON.stringify({imagemodifservice : imagemodifservice, nommodifservice : nommodifservice , descriptionmodifservice : descriptionmodifservice,   anciennom: anciennom }),})
+        const jsonreponseModifservice = await reponseModifservice.json();
+   
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+//maintenant pour les habitats
+
+const [imageajouthabitat, setImageajouthabitat] = useState('');
+const [nomajouthabitat, setNomajouthabitat] = useState('');
+const [descriptionajouthabitat, setDescriptionajouthabitat] = useState('');
+const [supprimhabitat, setSupprimhabitat] = useState('');
+const [imagemodifhabitat, setImagemodifhabitat] = useState('');
+const [nommodifhabitat, setNommodifhabitat] = useState('');
+const [descriptionmodifhabitat, setDescriptionmodifhabitat] = useState('');
+const [anciennomhabitat, setAnciennomhabitat] = useState('');
 
 
+// Ajouter habitat
+const handlerChangerimageajouthabitat = (event) => { setImageajouthabitat(event.target.value); }
+const handleChangernomajouthabitat = (event) => { setNomajouthabitat(event.target.value); }
+const handleChangerdescriptionajouthabitat = (event) => { setDescriptionajouthabitat(event.target.value); }
+
+// Supprimer habitat
+const handleChangersupprimhabitat = (event) => { setSupprimhabitat(event.target.value); }
+
+// Modifier habitat
+const handlerChangerimageModifhabitat = (event) => { setImagemodifhabitat(event.target.value); }
+const handleChangernommodifhabitat = (event) => { setNommodifhabitat(event.target.value); }
+const handleChangerdescriptionmodifhabitat = (event) => { setDescriptionmodifhabitat(event.target.value); }
+const handleChangeranciennomhabitat = (event) => { setAnciennomhabitat(event.target.value); }
+
+
+async function Ajouthabitat() {
+  try {
+    const reponseAjouthabitat = await fetch("http://localhost:8082/3000/Admins/habitat", {
+      credentials: "include", method: "post", headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({imageajouthabitat: imageajouthabitat,nomajouthabitat: nomajouthabitat,descriptionajouthabitat: descriptionajouthabitat }),
+    });
+    const jsonreponseAjouthabitat = await reponseAjouthabitat.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function Supprhabitat() {
+  try {
+    const responseSupprhabitat = await fetch("http://localhost:8082/3000/Admins/habitat", {credentials: "include",method: "delete", headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ supprimhabitat: supprimhabitat }),});
+    const jsonresponseSupprhabitat = await responseSupprhabitat.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function Modifhabitat() {
+  try {
+    const reponseModifhabitat = await fetch("http://localhost:8082/3000/Admins/habitat", { credentials: "include", method: "put",headers: { 'Content-Type': 'application/json' },
+       body: JSON.stringify({imagemodifhabitat: imagemodifhabitat,nommodifhabitat: nommodifhabitat,descriptionmodifhabitat: descriptionmodifhabitat,anciennomhabitat: anciennomhabitat}),
+    });
+    const jsonreponseModifhabitat = await reponseModifhabitat.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
   return (
     <div className="Container-Admins">
         <h1>Vous êtes connecté en tant qu'admin</h1>
-        <h3>Modification service</h3>
-        <form className="Ajout-service" onSubmit={(event) => {event.preventDefault(); Ajoutservice();}}>
+        <h3> Onglet service </h3>
+        <div className="service-onglet">
+        <div className="service-onglet1">
+        <form className="formclass" onSubmit={(event) => {event.preventDefault(); Ajoutservice();}}>
             <label htmlFor="image">Image URL</label>
             <textarea value={imageajoutservice} onChange={handlerChangerimageajservice} id="imageajoutservice"/>
             <label htmlFor="nom">Nom</label>
@@ -47,9 +143,71 @@ export default function Admins() {
             <input value={descriptionajoutservice} onChange={handleChangerdescriptionajoutservice} id="descriptionajoutservice"/>
             <button type="submit">Ajouter</button>
         </form>
+        </div>
        
-        <button>Modifier</button>
-        <button>Supprimer</button>
+        <div className="service-onglet2">
+        <form className="formclass"onSubmit={(event) => {event.preventDefault(); Supprservice();}}>
+        <label htmlFor="nom">Nom du service à supprimer</label>
+        <input value={supprimservice} onChange={handleChangersupprimmservice} id="supprimservice"/>
+        <button type="submit">Supprimer</button>
+        </form>
+        </div>
+        
+        <div className="service-onglet3">
+        <form className="formclass" onSubmit={(event) => {event.preventDefault(); Modifservice();}}>
+            <label htmlFor="anciennom">Nom actuel du service à modifier</label>
+            <input value={anciennom} onChange={handleChangeranciennom} id="anciennom" />
+            <label htmlFor="nom">Nouveau nom du service</label>
+            <input value={nommodifservice} onChange={handleChangernommodifservice} id="nommodifservice"/>
+            <label htmlFor="image">Image URL</label>
+            <textarea value={imagemodifservice} onChange={handlerChangerimagemoservice} id="imagemodifservice"/>
+            <label htmlFor="description">Description</label>
+            <input value={descriptionmodifservice} onChange={handleChangerdescriptionmodifservice} id="descriptionmodifservice"/>
+            <button type="submit">Modifier</button>
+        </form>
+        </div>
+        </div>
+     <h3>Onglet Habitat</h3>
+    <div className="habitat-onglet">
+    <div className="habitat-onglet1">
+      <form className="formclass" onSubmit={(event) => { event.preventDefault(); Ajouthabitat(); }}>
+      <label htmlFor="image">Image URL</label>
+      <textarea value={imageajouthabitat} onChange={handlerChangerimageajouthabitat} id="imageajouthabitat" />
+      <label htmlFor="nom">Nom</label>
+       <input value={nomajouthabitat} onChange={handleChangernomajouthabitat} id="nomajouthabitat" />
+       <label htmlFor="description">Description</label>
+      <input value={descriptionajouthabitat} onChange={handleChangerdescriptionajouthabitat} id="descriptionajouthabitat" />
+      <button type="submit">Ajouter</button>
+      </form>
     </div>
+
+
+    <div className="habitat-onglet2">
+     <form className="formclass" onSubmit={(event) => { event.preventDefault(); Supprhabitat(); }}>
+     <label htmlFor="nom">Nom du habitat à supprimer</label>
+     <input value={supprimhabitat} onChange={handleChangersupprimhabitat} id="supprimhabitat" />
+     <button type="submit">Supprimer</button>
+     </form>
+    </div>
+
+
+    <div className="habitat-onglet3">
+     <form className="formclass" onSubmit={(event) => { event.preventDefault(); Modifhabitat(); }}>
+     <label htmlFor="anciennom">Nom actuel du habitat à modifier</label>
+     <input value={anciennomhabitat} onChange={handleChangeranciennomhabitat} id="anciennomhabitat" />
+     <label htmlFor="nom">Nouveau nom du habitat</label>
+     <input value={nommodifhabitat} onChange={handleChangernommodifhabitat} id="nommodifhabitat" />
+     <label htmlFor="image">Image URL</label>
+     <textarea value={imagemodifhabitat} onChange={handlerChangerimageModifhabitat} id="imagemodifhabitat" />
+     <label htmlFor="description">Description</label>
+     <input value={descriptionmodifhabitat} onChange={handleChangerdescriptionmodifhabitat} id="descriptionmodifhabitat" />
+     <button type="submit">Modifier</button>
+   </form>
+    </div>
+       
+ </div>
+  
+       
+</div>
   )
 }
